ddc9a767b1a0d2252c9fa18f4f97f50e72f245f8
Mohsen Mehrani <m.mehrani14@gmail.com>
2019-11-12 21:31:11 +0330

    new tool successfully developed

--- Analysis_Tools_Homans.py
+++ Analysis_Tools_Homans.py
@@ -729,19 +729,22 @@ class Tracker: #XXX
         return correlation
     
     def correlation_growth_situation(self,survey_property_id,base_property_id):
+        
         survey_ref = {'money':self.agents_money,'approval':self.agents_approval,'asset':self.agents_asset}
         base_ref = { base_property_id:self._array(base_property_id)}
-        for key in survey_property_id:
-            base_ref['initial_'+key] = survey_ref[key][0,:]
         
-        fig = plt.figure()
-        ax = fig.subplot(111)
+        for key in survey_ref.keys():
+            base_ref['initial_'+ key] = survey_ref[key][0,:]
         
+        fig, ax = plt.subplots(nrows=1,ncols=1)
+        
+        survey_arr = survey_ref[survey_property_id]
+        base_arr = base_ref[base_property_id]
         for t in np.arange(self.T):
-            ax.scatter(t,np.corrcoef(survey_ref[survey_property_id][t,:],base_property_id[base_property_id])[0,1])
+            ax.scatter( [t],[ np.corrcoef(survey_arr[t,:]-survey_arr[0,:],base_arr[:])[0,1] ],c = 'b' )
         
         ax.set_title('correlation between '+survey_property_id+' & '+base_property_id)
-        fig.savefig('correlation between '+survey_property_id+' & '+base_property_id)
+        fig.savefig(self.path + 'correlation between '+survey_property_id+' & '+base_property_id)
         
         return
         
