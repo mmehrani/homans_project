f5c79d2f061b8ea524f6b9595c8c92b0e7c9de0e
Mohsen Mehrani <m.mehrani14@gmail.com>
2020-01-22 08:24:12 +0330

    trans results in lower approval than 10e-3 stopped

--- Homans.py
+++ Homans.py
@@ -187,7 +187,7 @@ def transaction(index1,index2,t,init=False):
         else:
             acceptance_util = 1
     
-        if agent2.approval > 0.001:
+        if agent2.approval > 0.001 and agent2.approval - ( np.round(amount*worth_ratio1 + agreement_point,3) ) > 0.001:
             if worth_ratio2 >= worth_ratio1:
                 acceptance_worth = 1
             else:
@@ -402,8 +402,8 @@ def save_it(version,t,boolean):
 # =============================================================================
 """Parameters"""#XXX
 
-N = 50
-T = 500
+N = 100
+T = 3000
 similarity = 0.05                   #how much this should be?
 memory_size = 10                    #contains the last memory_size number of transaction times
 transaction_percentage = 0.1        #percent of amount of money the first agent proposes from his asset 
@@ -417,7 +417,7 @@ alpha = 1                           #in short-term effect of the frequency of tr
 beta = 0.3                          #in long-term effect of the frequency of transaction
 param = 2                           #a normalizing factor in assigning the acceptance probability. It normalizes difference of money of both sides
 lamda = 0                           # how much one agent relies on his last worth_ratio and how much relies on current transaction's worth_ratio
-sampling_time = 100
+sampling_time = 1000
 if sampling_time > T:
     sampling_time = T
 
@@ -444,7 +444,7 @@ for i in np.arange(N):
 global tracker #made global to be reseted in related func
 global num_transaction_tot,explore_prob_array,saving_time_step
 
-saving_time_step = 100
+saving_time_step = 3000
 if saving_time_step < sampling_time:
     saving_time_step = sampling_time
 #explore_prob_array = np.zeros(saving_time_step)
